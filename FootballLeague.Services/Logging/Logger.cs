9c3bc1df0bf4d0294ca6376ea1321676c6a134d7
DSMarkoff <dsmarkov1@gmail.com>
2022-09-18 00:32:19 +0300

    Remove magic string in Logger

--- FootballLeague.Services/Logging/Logger.cs
+++ FootballLeague.Services/Logging/Logger.cs
@@ -5,11 +5,13 @@ namespace FootballLeague.Services.Logging
 {
     public static class Logger
     {
+        private const string COMPLETE_FILE_NAME = "Logs.txt";
+
         public static void Log(string message)
         {
             try
             {
-                using var sw = new StreamWriter("Logs.txt", true);
+                using var sw = new StreamWriter(COMPLETE_FILE_NAME, true);
                 sw.WriteLine(message);
             }
             catch (Exception)
