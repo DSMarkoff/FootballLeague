2b7900aec11b07a0d561490d996d4c72a50466b6
DSMarkoff <dsmarkov1@gmail.com>
2022-09-17 21:26:40 +0300

    Add TeamsController and remove redundant code

---
+++ FootballLeague/Controllers/TeamsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FootballLeague.Controllers
+{
+    public class TeamsController : ApiControllerBase
+    {
+        public TeamsController()
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update()
+        {
+
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Remove()
+        {
+
+        }
+    }
+}
